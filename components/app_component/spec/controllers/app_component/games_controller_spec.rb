c2d397efcad09fe27c9eee15028acda442350181
Andrew W. Lee <git@drewlee.com>
2020-08-15 11:13:12 -0700

    Add whitespace

--- components/app_component/spec/controllers/app_component/games_controller_spec.rb
+++ components/app_component/spec/controllers/app_component/games_controller_spec.rb
@@ -19,6 +19,7 @@ module AppComponent
         get :index, params: {}, session: valid_session
         expect(assigns(:games)).to eq [game]
       end
+
       it "routes to #index" do
         expect(:get => "/games").to route_to("app_component/games#index")
       end
@@ -30,25 +31,30 @@ module AppComponent
         get :show, params: {:id => game.to_param}, session: valid_session
         expect(assigns(:game)).to eq game
       end
+
       it "routes to #show" do
         expect(:get => "/games/1").to route_to("app_component/games#show", :id => "1")
       end
     end
+
     describe "GET new" do
       it "assigns a new game as @game" do
         get :new, params: {}, session: valid_session
         expect(assigns(:game)).to be_a_new AppComponent::Game
       end
+
       it "routes to #new" do
         expect(:get => "/games/new").to route_to("app_component/games#new")
       end
     end
+
     describe "GET edit" do
       it "assigns the requested game as @game" do
         game = create_game
         get :edit, params: {:id => game.to_param}, session: valid_session
         expect(assigns(:game)).to eq game
       end
+
       it "routes to #edit" do
         expect(:get => "/games/1/edit").to route_to("app_component/games#edit", :id => "1")
       end
@@ -61,24 +67,29 @@ module AppComponent
             post :create, params: {:game => valid_attributes}, session: valid_session
           }.to change(AppComponent::Game, :count).by 1
         end
+
         it "assigns a newly created game as @game" do
           post :create, params: {:game => valid_attributes}, session: valid_session
           expect(assigns(:game)).to be_a AppComponent::Game
           expect(assigns(:game)).to be_persisted
         end
+
         it "redirects to the created game" do
           post :create, params: {:game => valid_attributes}, session: valid_session
           expect(response).to redirect_to AppComponent::Game.last
         end
+
         it "routes to #create" do
           expect(:post => "/games").to route_to("app_component/games#create")
         end
       end
+
       describe "with invalid params" do
         it "assigns a newly created but unsaved game as @game" do
           post :create, params: {:game => invalid_attributes}, session: valid_session
           expect(assigns(:game)).to be_a_new AppComponent::Game
         end
+
         it "re-renders the new template" do
           post :create, params: {:game => invalid_attributes}, session: valid_session
           expect(response).to render_template "new"
@@ -95,26 +106,31 @@ module AppComponent
           game.reload
           expect(assigns(:game).date).to be_within(1).of new_time
         end
+
         it "assigns the requested game as @game" do
           game = create_game
           put :update, params: {:id => game.to_param, :game => valid_attributes}, session: valid_session
           expect(assigns(:game)).to eq game
         end
+
         it "redirects to the game" do
           game = create_game
           put :update, params: {:id => game.to_param, :game => valid_attributes}, session: valid_session
           expect(response).to redirect_to game
         end
+
         it "routes to #update" do
           expect(:put => "/games/1").to route_to("app_component/games#update", :id => "1")
         end
       end
+
       describe "with invalid params" do
         it "assigns the game as @game" do
           game = create_game
           put :update, params: {:id => game.to_param, :game => invalid_attributes}, session: valid_session
           expect(assigns(:game)).to eq game
         end
+
         it "re-renders the edit template" do
           game = create_game
           put :update, params: {:id => game.to_param, :game => invalid_attributes}, session: valid_session
@@ -130,11 +146,13 @@ module AppComponent
           delete :destroy, params: {:id => game.to_param}, session: valid_session
         }.to change(AppComponent::Game, :count).by -1
       end
+
       it "redirects to the games list" do
         game = create_game
         delete :destroy, params: {:id => game.to_param}, session: valid_session
         expect(response).to redirect_to games_url
       end
+
       it "routes to #destroy" do
         expect(:delete => "/games/1").to route_to("app_component/games#destroy", :id => "1")
       end
