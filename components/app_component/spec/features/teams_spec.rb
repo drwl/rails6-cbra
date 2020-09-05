848d780cd3da2a080ba9d01c2eac13f6a5d875a0
Andrew W. Lee <git@drewlee.com>
2020-09-05 12:11:28 -0700

    Namespace feature specs for consistency

--- components/app_component/spec/features/teams_spec.rb
+++ components/app_component/spec/features/teams_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe "teams admin", type: :feature do
-  it "allows for the management of teams" do
-    visit "/app_component/teams"
+module AppComponent
+  RSpec.describe "teams admin", type: :feature do
+    it "allows for the management of teams" do
+      visit "/app_component/teams"
 
-    click_link "New Team"
+      click_link "New Team"
 
-    fill_in "Name", with: "UofL"
-    click_on "Create Team"
+      fill_in "Name", with: "UofL"
+      click_on "Create Team"
 
-    click_link "New Team"
+      click_link "New Team"
 
-    fill_in "Name", with: "UK"
-    click_on "Create Team"
+      fill_in "Name", with: "UK"
+      click_on "Create Team"
 
-    expect(page).to have_content "UofL"
-    expect(page).to have_content "UK"
+      expect(page).to have_content "UofL"
+      expect(page).to have_content "UK"
+    end
   end
 end
